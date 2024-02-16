49b9a0cf1d4bc85b54aa305c36998a0fd2f7675d
layasp <122000138+layasp@users.noreply.github.com>
2024-02-17 00:40:16 +0530

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,14 +1,10 @@
-var clickcount = 0;
     function sub() {
         const name = document.getElementById("mongo").value;
-        if (name == "atlas" || name == "Atlas" || name == "ATLAS") {
-            console.log("Your next location is The Orb");
-            if (clickcount == "3") {
-        document.getElementById("sub").innerHTML = "Check your Console!!";
-    }
+        if (name == "atlas" || name == "Atlas" || name == "ATLAS") {    
+        document.getElementById("sub").innerHTML = "Your next location is The Orb";
         }
         else {
-            console.log("Try again <3");
+            document.getElementById("sub").innerHTML =("Try again <3");
             document.getElementById("mongo").value="";
         }
         clickcount = clickcount + 1;
